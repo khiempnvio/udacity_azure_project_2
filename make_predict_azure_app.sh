4d7fa6c98c4583fc4b95fdc6cc81b74572af5310
khiempn <khiempn@hotmail.com>
2024-06-04 20:11:32 +0700

    update host name

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://khiempnudacity.azurewebsites.net:$PORT/predict 
+     -X POST https://localhost.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
